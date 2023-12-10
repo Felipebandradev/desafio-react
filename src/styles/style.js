87455986e64e7d2e767166362fea519470a82c7b
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-12-10 17:52:14 -0300

    refactory: ajustes no conteúdo para telas pequenas

--- src/styles/style.js
+++ src/styles/style.js
@@ -4,8 +4,10 @@ import styled from "styled-components";
 /* inicio da estilização do Menu */
 const StyledMenu = styled.nav`
   margin: 2rem 0;
-
+  display: flex;
+  flex-direction: column;
   
+
   a {
     text-decoration: none;
     text-transform: uppercase;
@@ -13,7 +15,7 @@ const StyledMenu = styled.nav`
     color: black;
     margin: 2rem;
     padding: 1.99rem;
-    border-radius:15px 15px 0 0;
+    border-radius:   15px 0 0 15px    ;
   }
 
   a:hover {
@@ -31,13 +33,12 @@ const StyledMenu = styled.nav`
 const StlyedSection = styled.section`
   animation-name: animaContainer;
   animation-duration: 0.2s;
+ 
+
 
   @keyframes animaContainer {
     from {
       transform: scale(0.1);
-    }
-    to {
-      transform: scale(1);
     }
   }
 `; /* Fim das animações da sctions */
@@ -45,16 +46,17 @@ const StlyedSection = styled.section`
 /* inicio da estilização do Container */
 const StyledContainer = styled.main`
   background-color: #fff;
+  max-width: 100%;
   width: 60vw;
   height: 60vh;
+  overflow-y: scroll;
   border-radius: 0 0 15px 15px;
-  box-shadow: 5px 5px 20px black ;
+  box-shadow: 5px 5px 20px black;
 
   border: black solid 10px;
   h1 {
     text-align: center;
   }
-  
 
   h1,
   h2,
